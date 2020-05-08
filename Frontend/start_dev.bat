8e591f0953e6eebeba32fee38b9c529665cd8454
juandara <juan.ramirez.villegas@correounivalle.edu.co>
2020-05-08 00:46:29 -0500

    Some buggs with the docker deploy and the email connection

--- Frontend/start_dev.bat
+++ Frontend/start_dev.bat
@@ -2,12 +2,12 @@
 ::create the env vars:
 ::no needed yet
 set PRODUCTION=false
-set NODE_PORT=1000
+set NODE_PORT=8080
 set PORT=3000
 
 ::this format is needed to pass env vars to react 
-set PRODUCTION=true
-set REACT_APP_NODE_PORT=1000
+set REACT_APP_PRODUCTION=false
+set REACT_APP_NODE_PORT=8080
 
 ::start the backend on node
 start nodemon ./server.js
